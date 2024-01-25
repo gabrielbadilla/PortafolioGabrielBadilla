342dc906ee8c606aa9545c70376c2ab159c81895
gabrielbadilla <gbadilla60348@ufide.ac.cr>
2024-01-24 18:33:55 -0600

    creacion de proyecto

---
+++ Tienda_GabrielBadilla/src/test/java/tienda/demo/TiendaGabrielBadillaApplicationTests.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaGabrielBadillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
