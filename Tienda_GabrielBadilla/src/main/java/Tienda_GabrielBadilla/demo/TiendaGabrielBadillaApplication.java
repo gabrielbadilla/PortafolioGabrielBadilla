e4cf99bfdb63d39100fd9a9cca64b046f974e02a
gabrielbadilla <gbadilla60348@ufide.ac.cr>
2024-01-26 11:06:58 -0600

    creacion proyecto funcional
    se crea el proyecto funcional, comits anteriores no tomar en cuenta fueron del error de localhost en clases, initial comit si tomar en cuenta

---
+++ Tienda_GabrielBadilla/src/main/java/Tienda_GabrielBadilla/demo/TiendaGabrielBadillaApplication.java
@@ -0,0 +1,13 @@
+package Tienda_GabrielBadilla.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaGabrielBadillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaGabrielBadillaApplication.class, args);
+	}
+
+}
